0aa5971056795eb826d64779497b7126c1eded46
Chris Flinn <christopher.flinn@gmail.com>
2024-05-11 12:10:38 +1000

    sauce

---
+++ main.zig
@@ -0,0 +1,16 @@
+//
+//  _     _     _
+// | |   (_)___| |_ ___ _ __   ___ _ __
+// | |   | / __| __/ _ \ '_ \ / _ \ '__|
+// | |___| \__ \ ||  __/ | | |  __/ |
+// |_____|_|___/\__\___|_| |_|\___|_|
+//
+// This little dude just relaxes and listens
+// to the files stream in from the void.
+//
+const std = @import("std");
+
+pub fn main() void {
+    // listen to incoming traffic on a specified port
+    //
+}
---
+++ pdf.zig
@@ -0,0 +1,13 @@
+//
+//            _  __
+//  _ __   __| |/ _|
+// | '_ \ / _` | |_
+// | |_) | (_| |  _|
+// | .__/ \__,_|_|
+// |_|
+//
+// pdf sanitizater.
+//
+const std = @import("std");
+
+pub fn pdfSanitize() void {}
